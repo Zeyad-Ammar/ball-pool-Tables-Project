e5cb17289eb2e797fddaffd8a8ca3379a2a7b073
Zeyad Ammar <zeyadammar2510@gmail.com>
2026-09-12 14:33:32 +0300

    add 8 tables to the project

--- poolTables/Form1.Designer.cs
+++ poolTables/Form1.Designer.cs
@@ -30,27 +30,114 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.ctrlPoolTable1 = new poolTables.ctrlPoolTable();
+            this.ctrlPoolTable2 = new poolTables.ctrlPoolTable();
+            this.ctrlPoolTable3 = new poolTables.ctrlPoolTable();
+            this.ctrlPoolTable4 = new poolTables.ctrlPoolTable();
+            this.ctrlPoolTable5 = new poolTables.ctrlPoolTable();
+            this.ctrlPoolTable6 = new poolTables.ctrlPoolTable();
+            this.ctrlPoolTable7 = new poolTables.ctrlPoolTable();
+            this.ctrlPoolTable8 = new poolTables.ctrlPoolTable();
             this.SuspendLayout();
             // 
             // ctrlPoolTable1
             // 
-            this.ctrlPoolTable1.hourlyRate = 100;
+            this.ctrlPoolTable1.hourlyRate = 10;
             this.ctrlPoolTable1.Location = new System.Drawing.Point(1, 1);
             this.ctrlPoolTable1.Name = "ctrlPoolTable1";
-            this.ctrlPoolTable1.playerName = "player 1";
+            this.ctrlPoolTable1.playerName = "Player Name";
             this.ctrlPoolTable1.Size = new System.Drawing.Size(331, 342);
             this.ctrlPoolTable1.TabIndex = 0;
             this.ctrlPoolTable1.tableName = "Tabel 1";
+            // 
+            // ctrlPoolTable2
+            // 
+            this.ctrlPoolTable2.hourlyRate = 10;
+            this.ctrlPoolTable2.Location = new System.Drawing.Point(333, 1);
+            this.ctrlPoolTable2.Name = "ctrlPoolTable2";
+            this.ctrlPoolTable2.playerName = "player";
+            this.ctrlPoolTable2.Size = new System.Drawing.Size(331, 342);
+            this.ctrlPoolTable2.TabIndex = 1;
+            this.ctrlPoolTable2.tableName = "Table 2";
+            // 
+            // ctrlPoolTable3
+            // 
+            this.ctrlPoolTable3.hourlyRate = 10;
+            this.ctrlPoolTable3.Location = new System.Drawing.Point(666, 1);
+            this.ctrlPoolTable3.Name = "ctrlPoolTable3";
+            this.ctrlPoolTable3.playerName = "Player Name";
+            this.ctrlPoolTable3.Size = new System.Drawing.Size(331, 342);
+            this.ctrlPoolTable3.TabIndex = 2;
+            this.ctrlPoolTable3.tableName = "Table 3";
+            // 
+            // ctrlPoolTable4
+            // 
+            this.ctrlPoolTable4.hourlyRate = 10;
+            this.ctrlPoolTable4.Location = new System.Drawing.Point(998, 1);
+            this.ctrlPoolTable4.Name = "ctrlPoolTable4";
+            this.ctrlPoolTable4.playerName = "player";
+            this.ctrlPoolTable4.Size = new System.Drawing.Size(331, 342);
+            this.ctrlPoolTable4.TabIndex = 3;
+            this.ctrlPoolTable4.tableName = "Table 4";
+            // 
+            // ctrlPoolTable5
+            // 
+            this.ctrlPoolTable5.hourlyRate = 10;
+            this.ctrlPoolTable5.Location = new System.Drawing.Point(1, 344);
+            this.ctrlPoolTable5.Name = "ctrlPoolTable5";
+            this.ctrlPoolTable5.playerName = "player";
+            this.ctrlPoolTable5.Size = new System.Drawing.Size(331, 342);
+            this.ctrlPoolTable5.TabIndex = 4;
+            this.ctrlPoolTable5.tableName = "Table 5";
+            // 
+            // ctrlPoolTable6
+            // 
+            this.ctrlPoolTable6.hourlyRate = 10;
+            this.ctrlPoolTable6.Location = new System.Drawing.Point(333, 344);
+            this.ctrlPoolTable6.Name = "ctrlPoolTable6";
+            this.ctrlPoolTable6.playerName = "player";
+            this.ctrlPoolTable6.Size = new System.Drawing.Size(331, 342);
+            this.ctrlPoolTable6.TabIndex = 5;
+            this.ctrlPoolTable6.tableName = "Table 6";
+            // 
+            // ctrlPoolTable7
+            // 
+            this.ctrlPoolTable7.hourlyRate = 10;
+            this.ctrlPoolTable7.Location = new System.Drawing.Point(666, 344);
+            this.ctrlPoolTable7.Name = "ctrlPoolTable7";
+            this.ctrlPoolTable7.playerName = "player";
+            this.ctrlPoolTable7.Size = new System.Drawing.Size(331, 342);
+            this.ctrlPoolTable7.TabIndex = 6;
+            this.ctrlPoolTable7.tableName = "Table 7";
+            // 
+            // ctrlPoolTable8
+            // 
+            this.ctrlPoolTable8.hourlyRate = 10;
+            this.ctrlPoolTable8.Location = new System.Drawing.Point(998, 344);
+            this.ctrlPoolTable8.Name = "ctrlPoolTable8";
+            this.ctrlPoolTable8.playerName = "player";
+            this.ctrlPoolTable8.Size = new System.Drawing.Size(331, 342);
+            this.ctrlPoolTable8.TabIndex = 7;
+            this.ctrlPoolTable8.tableName = "Table 8";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::poolTables.Properties.Resources.saint_rambo_BhvoOhQMv5I_unsplash;
+            this.ClientSize = new System.Drawing.Size(1333, 690);
+            this.Controls.Add(this.ctrlPoolTable8);
+            this.Controls.Add(this.ctrlPoolTable7);
+            this.Controls.Add(this.ctrlPoolTable6);
+            this.Controls.Add(this.ctrlPoolTable5);
+            this.Controls.Add(this.ctrlPoolTable4);
+            this.Controls.Add(this.ctrlPoolTable3);
+            this.Controls.Add(this.ctrlPoolTable2);
             this.Controls.Add(this.ctrlPoolTable1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "كوكب البليردو";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
@@ -58,6 +145,13 @@
         #endregion
 
         private ctrlPoolTable ctrlPoolTable1;
+        private ctrlPoolTable ctrlPoolTable2;
+        private ctrlPoolTable ctrlPoolTable3;
+        private ctrlPoolTable ctrlPoolTable4;
+        private ctrlPoolTable ctrlPoolTable5;
+        private ctrlPoolTable ctrlPoolTable6;
+        private ctrlPoolTable ctrlPoolTable7;
+        private ctrlPoolTable ctrlPoolTable8;
     }
 }
 
